7b68fde3efe930910b5f8ed497c5df0e8edd4d88
ITharu123 <tharushiranaweera2071@gmail.com>
2021-08-21 16:56:35 +0530

    new commit

---
+++ app/src/main/java/com/example/tute_4/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.tute_4;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.DirectAction;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity<requestCode> extends AppCompatActivity {
+    ImageView imageView;
+    Button btOpen;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image_view);
+        btOpen = findViewById(R.id.bt_open);
+
+
+        if(ContextCompat.checkSelfPermission(MainActivity. this, Manifest.permission.CAMERA)!=
+        PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            },
+                    100);
+
+        }
+        btOpen.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(captureImage);
+        }
+    }
+  }
